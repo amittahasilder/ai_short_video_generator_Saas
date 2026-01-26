34e202e56408c513673c5483d6613fba5909c11f
amittahasilder <amittahasilder@gmail.com>
2026-01-25 21:29:35 -0800

    your message

--- app/dashboard/_components/EmptyState.jsx
+++ app/dashboard/_components/EmptyState.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Button } from "../../../components/ui/button"
+import React from "react"
 
 function EmptyState() {
   return (
-    <div>EmptyState</div>
+    <div className="p-5 border-dotted border-amber-600 py-24 flex flex-col items-center gap-4 ">
+      <div>You don’t have any short video created</div>
+      <Button>Create New Short Video</Button>
+    </div>
   )
 }
 
