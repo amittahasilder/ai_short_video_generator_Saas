a9becb85937cc0062d52a59c365475e91eeb1c6c
amittahasilder <amittahasilder@gmail.com>
2026-01-26 01:36:31 -0800

    your message

--- app/dashboard/_components/EmptyState.jsx
+++ app/dashboard/_components/EmptyState.jsx
@@ -6,7 +6,7 @@ function EmptyState() {
   return (
     <div className="p-5 border-dotted border-amber-600 py-24 flex flex-col items-center gap-4 ">
      <h2>You Dont have any short video Created </h2>
-     <Link href={'/create-new'}>
+     <Link href={'/dashboard/create-new'}>
 
 
       <Button>Create New Short Video</Button>
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,21 +1,56 @@
+// "use client"
+
+// import React, { useState } from "react"
+// import { Button } from "../../components/ui/button"
+// import EmptyState from "./_components/EmptyState"
+// import CreateNew from './creat-new/page';
+// import { Link } from "lucide-react";
+
+// const Dashboard = () => {
+//   const [videoList, setVideoList] = useState([])
+
+//   return (
+//     <>
+//       <div className="home flex justify-between items-center bg-orange-300">
+//         <h2>Dashboard</h2>
+//              <Link href={'/app/dashboard/creat-new/page.jsx'}>
+        
+        
+//               <Button>+ CreateNew</Button>
+//               </Link>
+//       </div>
+
+//       {/* Empty state */}
+//       {videoList.length === 0 && (
+//         <div>
+//           <EmptyState />
+//         </div>
+//       )}
+//     </>
+//   )
+// }
+
+// export default Dashboard
+
+
 "use client"
 
 import React, { useState } from "react"
 import { Button } from "../../components/ui/button"
 import EmptyState from "./_components/EmptyState"
+import Link from "next/link"
 
 const Dashboard = () => {
   const [videoList, setVideoList] = useState([])
 
   return (
     <>
-      <div className="home flex justify-between items-center bg-orange-300">
-        <h2>Dashboard</h2>
-             <Link href={'/create-new'}>
-        
-        
-              <Button>Create New Short Video</Button>
-              </Link>
+      <div className="home flex justify-between items-center bg-orange-300 p-4">
+        <h2 className="text-xl font-bold">Dashboard</h2>
+
+        <Link href="/dashboard/creat-new">
+          <Button>+ Create New</Button>
+        </Link>
       </div>
 
       {/* Empty state */}
